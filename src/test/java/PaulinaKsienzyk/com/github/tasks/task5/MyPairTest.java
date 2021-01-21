23ed08fa4367ef4dcb03ec82c99f89a4e77c33ed
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-01-21 22:41:21 +0100

    Add MyPair tests

---
+++ src/test/java/PaulinaKsienzyk/com/github/tasks/task5/MyPairTest.java
@@ -0,0 +1,32 @@
+package PaulinaKsienzyk.com.github.tasks.task5;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Objects;
+
+import static org.testng.Assert.*;
+
+public class MyPairTest {
+
+    private final Sock sock1 = new Sock();
+    private final Sock sock2 = new Sock();
+
+    @Test
+    public void shouldAcceptTwoItemsOfTheSameType() {
+        MyPair<Sock,Sock> socksPair = new MyPair<>(sock1,sock2);
+
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(Objects.nonNull(socksPair.getKey()));
+        sa.assertTrue(Objects.nonNull(socksPair.getValue()));
+
+        sa.assertAll();
+    }
+
+    @Test
+    public void shouldNotAcceptsNulls() {
+        assertThrows(NullPointerException.class, () -> new MyPair<>(null, null));
+    }
+
+
+}
