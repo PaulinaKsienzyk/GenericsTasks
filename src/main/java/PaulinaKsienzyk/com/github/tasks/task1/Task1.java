c5390f56d88d6438f5932cd32b11e1c264c2d28d
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-01-21 21:13:10 +0100

    Add Task1

---
+++ src/main/java/PaulinaKsienzyk/com/github/tasks/task1/Task1.java
@@ -0,0 +1,14 @@
+package PaulinaKsienzyk.com.github.tasks.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        String s = list.get(0);
+    }
+
+}
