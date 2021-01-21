f1a2ae2142269b262eed5ad7c07a48da330f6411
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-01-21 21:13:19 +0100

    Add Task2

---
+++ src/main/java/PaulinaKsienzyk/com/github/tasks/task2/Task2.java
@@ -0,0 +1,12 @@
+package PaulinaKsienzyk.com.github.tasks.task2;
+
+public class Task2 {
+
+        class Box<T> {
+            private T object;
+
+            void set(T object) {this.object = object; }
+            T get() { return object; }
+        }
+
+}
